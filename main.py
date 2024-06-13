74a706b08c019f2aaa71016f4dc5cc6551c740bf
Vinicius Martinez <vn.martinez@hotmail.com>
2024-06-13 08:47:19 -0300

    teste deploy azure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 import time
 import uuid
@@ -12,6 +12,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/upload', methods=['POST'])
 def upload_image():
     try:
@@ -23,6 +28,9 @@ def upload_image():
             return jsonify({'error': 'Nenhum arquivo selecionado para upload.'}), 400
 
         text = " Você deverá responder em Português do Brasil. Esta imagem contem a foto de um rótulo de vinho. Descreva qual vinho é, qual uva, quais as características do vinho e quais as harmonizações possíveis do vinho. Caso a imagem não seja um rótulo de vinho, informe que não pode reconhecer um rótulo de vinho na imagem. Retorne tudo em formato JSON"
+
+        if not os.path.exists('uploads'):
+            os.makedirs('uploads')
 
         timestamp = int(time.time())
         unique_id = str(uuid.uuid4())[:8]
@@ -70,6 +78,4 @@ def show_result():
     return render_template('result.html', result_text=result_text)
 
 if __name__ == '__main__':
-    if not os.path.exists('uploads'):
-        os.makedirs('uploads')
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
